5b2948409e841aa17cf77a1bdbe86c5fe8f15871
rustyralminar <alminar.rusty.r@gmail.com>
2022-01-29 14:20:59 +0800

    admin

---
+++ admin/include/nav.php
@@ -0,0 +1,67 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Environmental</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+  <!--     <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li> -->
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+      <!-- Side navigation -->
+        <div class="sidenav bg-light">
+
+            <div class="card bg-light" align="center" style="border:none;">
+              <center>
+              <img class="card-img-top" src="https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Clipart.png"  alt="Card image" style="width:80px">
+              </center>
+               <p class="card-text">POGI KO</p>
+            </div>
+<hr>
+          <ul class="nav flex-column">
+                <li class="nav-item">
+                  <a class="nav-link" href="home.php">Dashboard</a>
+                </li>
+          </ul>
+
+          &nbsp;<span class="text-secondary">Action</span>
+          <ul class="nav flex-column">
+                <li class="nav-item">
+                  <a class="nav-link" href="book_manage.php">Books</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="course_manage.php">Course</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+          </ul>
+  
+        </div>
+
